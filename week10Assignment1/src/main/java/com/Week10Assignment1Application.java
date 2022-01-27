a237cb88784a50dd9e6c5029803d993cec90ec83
deepika <deepikamogilipuri2000@gmail.com>
2022-01-27 15:51:58 +0530

    done

---
+++ week10Assignment1/src/main/java/com/Week10Assignment1Application.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week10Assignment1Application.class, args);
+		System.out.println("Hello DevOps");
+	}
+
+}
